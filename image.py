9939eb8def475896f71df3c2f415c3c89347535b
Swapnil Tamrakar <swappy208@gmail.com>
2017-12-01 17:45:15 -0500

    initialization

---
+++ image.py
@@ -0,0 +1,3 @@
+import urllib as library
+
+library.urlretrieve("https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAATQAAAAJGUyMDE2NTE2LTk4OGItNGRkNi1iYjhjLWI3YzMzYWU3YTAwZA.jpg", "image.jpg")
